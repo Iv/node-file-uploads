ad09ccbc96e42b91ab6ea5077765dce93888ce15
Томаш Хамлай <tomash.khamlai@gmail.com>
2016-09-30 16:16:33 +0300

    up

--- routes/index.js
+++ routes/index.js
@@ -5,33 +5,32 @@ var express = require('express'),
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Node.js File Uploads' });
+    res.render('index', { title: 'Node.js File Uploads' });
 });
 
 router.post('/', function(req, res, next) {
     // create a form to begin parsing
     var form = new multiparty.Form();
-    var uploadFile = {uploadPath: '', type: '', size: 0};
+    var uploadFile = { uploadPath: '', type: '', size: 0 };
     var maxSize = 2 * 1024 * 1024; //2MB
     var supportMimeTypes = ['image/jpg', 'image/jpeg', 'image/png'];
     var errors = [];
 
-    form.on('error', function(err){
-        if(fs.existsSync(uploadFile.path)) {
+    form.on('error', function(err) {
+        if (fs.existsSync(uploadFile.path)) {
             fs.unlinkSync(uploadFile.path);
             console.log('error');
         }
     });
 
     form.on('close', function() {
-        if(errors.length == 0) {
-            res.send({status: 'ok', text: 'Success'});
-        }
-        else {
-            if(fs.existsSync(uploadFile.path)) {
+        if (errors.length == 0) {
+            res.send({ status: 'ok', text: 'Success' });
+        } else {
+            if (fs.existsSync(uploadFile.path)) {
                 fs.unlinkSync(uploadFile.path);
             }
-            res.send({status: 'bad', errors: errors});
+            res.send({ status: 'bad', errors: errors });
         }
     });
 
@@ -39,22 +38,20 @@ router.post('/', function(req, res, next) {
     form.on('part', function(part) {
         uploadFile.size = part.byteCount;
         uploadFile.type = part.headers['content-type'];
-        uploadFile.path = '/app/files /
-' + part.filename;
+        uploadFile.path = '/app/files/' + part.filename;
 
-        if(uploadFile.size > maxSize) {
+        if (uploadFile.size > maxSize) {
             errors.push('File size is ' + uploadFile.size / 1024 / 1024 + '. Limit is' + (maxSize / 1024 / 1024) + 'MB.');
         }
 
-        if(supportMimeTypes.indexOf(uploadFile.type) == -1) {
+        if (supportMimeTypes.indexOf(uploadFile.type) == -1) {
             errors.push('Unsupported mimetype ' + uploadFile.type);
         }
 
-        if(errors.length == 0) {
+        if (errors.length == 0) {
             var out = fs.createWriteStream(uploadFile.path);
             part.pipe(out);
-        }
-        else {
+        } else {
             part.resume();
         }
     });
